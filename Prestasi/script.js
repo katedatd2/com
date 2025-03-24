1790900fe5439a05e503d8697b110f953b49d09e
katedatd2 <izaqi58@gmail.com>
2025-03-24 15:44:16 +0700

    Delete Prestasi directory

--- Prestasi/script.js
+++
@@ -1,9 +0,0 @@
-function startAudio() {
-    var audio = document.getElementById("myaudio");
-    audio.volume = 1.0;
-    audio.play().then(() => {
-        console.log("Audio berhasil diputar.");
-    }).catch((error) => {
-        console.error("Gagal memutar audio:", error);
-    });
-}
